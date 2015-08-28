dfaeea33c96eb293c7276d378f95a86eaffdde73
Giovanni Harting <giovanni.harting@touchdata.net>
2015-08-28 19:59:10 +0200

    some formatting changes and added missing licences

--- ledd/effects/fadeeffect.py
+++ ledd/effects/fadeeffect.py
@@ -1,5 +1,22 @@
-from ledd.effects.generatoreffect import GeneratorEffect
+# LEDD Project
+# Copyright (C) 2015 LEDD Team
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import spectra
+
+from ledd.effects.generatoreffect import GeneratorEffect
 
 
 class FadeEffect(GeneratorEffect):
